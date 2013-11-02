d8a6c32b32c734c1d1500d74d1e9e7f6dec91e47
Ishibashi Hideto <me@ishibashihideto.net>
2013-11-02 19:44:47 +0900

    implement ENML2Markdown#notes()

--- test/test.js
+++ test/test.js
@@ -3,9 +3,21 @@ var ENML2Markdown = require('../lib/enml2md.js'),
 
 describe('ENML2Markdown', function(){
   describe('#notes()', function(){
-    it('should return `2` for `fiture2.enex`.', function(){
-      var enml = new ENML2Markdown('fixtures/fixture2.enex');
-      enml.notes().should.equal(2);
+    it('should return `1` for `fiture1.enex`.', function(done){
+      var enml = new ENML2Markdown('./test/fixtures/fixture1.enex');
+      var count_async = enml.notes();
+      count_async.on('done', function(count) {
+        count.should.equal(1);
+        done();
+      });
+    });
+    it('should return `2` for `fiture2.enex`.', function(done){
+      var enml = new ENML2Markdown('./test/fixtures/fixture2.enex');
+      var count_async = enml.notes();
+      count_async.on('done', function(count) {
+        count.should.equal(2);
+        done();
+      });
     });
   })
 })

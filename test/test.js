29a214fd2a67c867debd78ab237d995a787aefb9
Ishibashi Hideto <me@ishibashihideto.net>
2013-11-03 02:16:46 +0900

    refactoring tests

--- test/test.js
+++ test/test.js
@@ -2,12 +2,9 @@ var enml2md = require('../lib/enml2md.js'),
   should = require('should'),
   fs = require('fs')
 
-describe('EvernoteExport', function(){
-  describe('#notesCount()', function(){
-    it('should return `1` for `fiture1.enex`.', function(done){
-      testNotesCount('./test/fixtures/fixture1.enex', 1, done);
-    });
-    it('should return `2` for `fiture2.enex`.', function(done){
+describe('EvernoteExport', function() {
+  describe('#notesCount()', function() {
+    it('should return a count of notes in a ENML(.enex) file.', function(done) {
       testNotesCount('./test/fixtures/fixture2.enex', 2, done);
     });
   });
@@ -22,21 +19,21 @@ function testNotesCount(enml_filename, expect, done) {
   });
 }
 
-describe('Note', function(){
+describe('Note', function() {
   var note_enml = fs.readFileSync('./test/fixtures/note.enex');
   var note = enml2md.Note.parse(note_enml);
-  it('.parseNote() should return a note object.', function(){
+  it('.parseNote() should return a note object.', function() {
     note.should.be.an.instanceof(enml2md.Note);
   });
-  it('#title should return a title.', function(){
+  it('#title should be a title.', function() {
     note.title.should.equal('a single note fixture');
   });
-  it('#created should return a date.', function(){
+  it('#created should be a date.', function() {
     var date = new Date(2013, 11, 2, 10, 7, 9); // 20131102T100709Z
     note.created.should.be.an.instanceof(Date);
     note.created.should.eql(date);
   });
-  it('#updated should return a date.', function(){
+  it('#updated should be a date.', function() {
     var date = new Date(2013, 11, 2, 10, 7, 13); // 20131102T100713Z
     note.updated.should.be.an.instanceof(Date);
     note.updated.should.eql(date);

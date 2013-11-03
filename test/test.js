89e37019d6ab43cf75e1b884a10f293ad4b17bd4
Ishibashi Hideto <me@ishibashihideto.net>
2013-11-04 06:10:34 +0900

    refactoring test

--- test/test.js
+++ test/test.js
@@ -98,35 +98,37 @@ describe('EvernoteExport(enml_filename)', function() {
 })
 
 describe('Note', function() {
-  var note_enml = fs.readFileSync('./test/fixtures/note.enex')
-  var note = enml2md.Note.parse(note_enml)
-  it('.parse() returns a note object.', function() {
-    note.should.be.an.instanceof(enml2md.Note)
-  })
-  it('#filename(extention) escapes title string.', function() {
-    note.filename('.md').should.equal('a single note fixture test  1.md')
-    note.filename('.txt').should.equal('a single note fixture test  1.txt')
-  })
-  it('#title is a title string.', function() {
-    note.title.should.equal('a single note fixture/test: 1')
-  })
-  it('#created is a Date object.', function() {
-    var date = new Date(2013, 10, 2, 10, 7-tzMin, 9) // 20131102T100709Z
-    note.created.should.be.an.instanceof(Date)
-    note.created.should.eql(date)
-  })
-  it('#updated is a Date object.', function() {
-    var date = new Date(2013, 10, 2, 10, 7-tzMin, 13) // 20131102T100713Z
-    note.updated.should.be.an.instanceof(Date)
-    note.updated.should.eql(date)
-  })
-  it('#tags is an array of string.', function() {
-    note.tags.should.be.an.instanceof(Array)
-    note.tags.length.should.equal(2)
-    note.tags[0].should.equal('markdown')
-    note.tags[1].should.equal('evernote')
-  })
-  it('#content is a Markdown string.', function() {
-    note.content.should.equal('fixture content\n\n')
+  describe('without resources', function () {
+    var note_enml = fs.readFileSync('./test/fixtures/note.enex')
+    var note = enml2md.Note.parse(note_enml)
+    it('.parse() returns a note object.', function() {
+      note.should.be.an.instanceof(enml2md.Note)
+    })
+    it('#filename(extention) escapes title string.', function() {
+      note.filename('.md').should.equal('a single note fixture test  1.md')
+      note.filename('.txt').should.equal('a single note fixture test  1.txt')
+    })
+    it('#title is a title string.', function() {
+      note.title.should.equal('a single note fixture/test: 1')
+    })
+    it('#created is a Date object.', function() {
+      var date = new Date(2013, 10, 2, 10, 7-tzMin, 9) // 20131102T100709Z
+      note.created.should.be.an.instanceof(Date)
+      note.created.should.eql(date)
+    })
+    it('#updated is a Date object.', function() {
+      var date = new Date(2013, 10, 2, 10, 7-tzMin, 13) // 20131102T100713Z
+      note.updated.should.be.an.instanceof(Date)
+      note.updated.should.eql(date)
+    })
+    it('#tags is an array of string.', function() {
+      note.tags.should.be.an.instanceof(Array)
+      note.tags.length.should.equal(2)
+      note.tags[0].should.equal('markdown')
+      note.tags[1].should.equal('evernote')
+    })
+    it('#content is a Markdown string.', function() {
+      note.content.should.equal('fixture content\n\n')
+    })
   })
 })

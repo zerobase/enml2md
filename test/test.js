ca27e4bde3b6640a9597be4c44a937d70735bff9
Ishibashi Hideto <me@ishibashihideto.net>
2013-11-03 17:30:00 +0900

    rewrite test descriptions: use `it('is something')` instead of `it('should be something')`

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var enml2md = require('../lib/enml2md.js'),
 
 describe('EvernoteExport(enml_filename)', function() {
   describe('#each(cbEach, cbEnd)', function() {
-    it('should set total #count at cbEnd()', function(done) {
+    it('sets total #count at cbEnd()', function(done) {
       var enex = new enml2md.EvernoteExport('./test/fixtures/fixture2.enex')
       var cbEach = function (note) { // callback for each note
         // do nothing
@@ -15,7 +15,7 @@ describe('EvernoteExport(enml_filename)', function() {
       }
       enex.each(cbEach, cbEnd)
     })
-    it('should call cbEach(note)', function(done) {
+    it('calls cbEach(note)', function(done) {
       var enex = new enml2md.EvernoteExport('./test/fixtures/fixture1.enex')
       var expected_created = new Date(2013, 11, 2, 10, 0, 55); // 20131102T100055Z
       var expected_updated = new Date(2013, 11, 2, 10, 3, 49); // 20131102T100349Z
@@ -40,29 +40,29 @@ describe('EvernoteExport(enml_filename)', function() {
 describe('Note', function() {
   var note_enml = fs.readFileSync('./test/fixtures/note.enex')
   var note = enml2md.Note.parse(note_enml)
-  it('.parse() should return a note object.', function() {
+  it('.parse() returns a note object.', function() {
     note.should.be.an.instanceof(enml2md.Note)
   })
-  it('#title should be a title.', function() {
+  it('#title is a title string.', function() {
     note.title.should.equal('a single note fixture')
   })
-  it('#created should be a date.', function() {
+  it('#created is a Date object.', function() {
     var date = new Date(2013, 11, 2, 10, 7, 9); // 20131102T100709Z
     note.created.should.be.an.instanceof(Date)
     note.created.should.eql(date)
   })
-  it('#updated should be a date.', function() {
+  it('#updated is a Date object.', function() {
     var date = new Date(2013, 11, 2, 10, 7, 13); // 20131102T100713Z
     note.updated.should.be.an.instanceof(Date)
     note.updated.should.eql(date)
   })
-  it('#tags should be an array of string.', function() {
+  it('#tags is an array of string.', function() {
     note.tags.should.be.an.instanceof(Array)
     note.tags.length.should.equal(2)
     note.tags[0].should.equal('markdown')
     note.tags[1].should.equal('evernote')
   })
-  it('#content should be a Markdown string.', function() {
+  it('#content is a Markdown string.', function() {
     note.content.should.equal('fixture content\n\n')
   })
 })
